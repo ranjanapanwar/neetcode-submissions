ee955a9f8a37ee987062e3a420015e881c0cf3dd
Ranjana Panwar <ranjanapanwar@gmail.com>
2026-06-24 20:39:17 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        for(int i=0, j=s.length()-1; i < j;){
+            char chI = s.charAt(i);
+            if(!((chI >= 'a' && chI <= 'z') || (chI >= '0' && chI <= '9'))){
+                i++;
+                continue;
+            }
+            char chJ = s.charAt(j);
+            if(!((chJ >= 'a' && chJ <= 'z') || (chJ >= '0' && chJ <= '9'))){
+                j--;
+                continue;
+            }
+
+            if(chI != chJ){
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+        
+    }
+}
